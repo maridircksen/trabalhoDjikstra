d53ba0c918606012e8596a2401abe9158a66f6a1
Julia Salvador <juliasalvador10@gmail.com>
2023-10-23 19:24:51 -0300

    Ajustando index

---
+++ dados.php
@@ -0,0 +1,23 @@
+<?php
+require_once "./classes/grafo.php";
+require_once "./classes/dijkstra.php";
+require_once "./classes/viewRotas.php";
+require_once "./classes/routes.php";
+
+$opcaoAeroporto = $_POST['aeroporto'];
+
+//Cria uma instância da classe grafo que contem as informações para o processamento.
+$oGrafo = new \grafo();
+// Cria uma instância da classe que realiza o calculo da informações setadas na classe grafo.
+$oDjikstra = new \djikstra();
+//Cria uma instância da classe responsável pela visualização das rotas.
+$oView = new \viewRotas();
+//Cria uma instância da classe responsável pela geração das strings de rotas.
+$oRoutes = new \routes();
+//Chama a função que calcula as rotas do grafo e passa o vértice inicial.
+$aRetorno = $oDjikstra->calcularRotas($oGrafo->getGrafo(), $opcaoAeroporto);
+//Monta as strings contendo as melhores rotas de acordo com o resultado do processamento.
+$aRotas = $oRoutes->getRotas($aRetorno);
+//Exibe o resultado na tela.
+$oView->exibeRotas($aRotas);
+
